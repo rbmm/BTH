d18dc44b4ce026237b8e2f38d02029d6bdf5c2b2
rbmm <_dm_sh_@tutanota.com>
2025-01-11 00:25:15 +0200

    *

--- udp.cpp
+++ udp.cpp
@@ -9,59 +9,58 @@ _NT_BEGIN
 
 BOOL OpenPort(INetFwOpenPorts* fwOpenPorts, LONG portNumber, NET_FW_IP_PROTOCOL ipProtocol)
 {
-  BOOL fOk = FALSE;
+	BOOL fOk = FALSE;
 
-  INetFwOpenPort* fwOpenPort;
+	INetFwOpenPort* fwOpenPort;
 
-  if (!CoCreateInstance(__uuidof(NetFwOpenPort), 0, CLSCTX_INPROC_SERVER, IID_PPV(fwOpenPort)))
-  {
-    if (BSTR fwBstrName = SysAllocString(L"{9F82CD7E-BA9F-48b7-BC82-02CCD524775A}"))
-    {
-      fOk = !fwOpenPort->put_Port(portNumber) &&
-        !fwOpenPort->put_Name(fwBstrName) &&
-        !fwOpenPort->put_Protocol(ipProtocol) &&
-        !fwOpenPorts->Add(fwOpenPort);
+	if (!CoCreateInstance(__uuidof(NetFwOpenPort), 0, CLSCTX_INPROC_SERVER, IID_PPV(fwOpenPort)))
+	{
+		if (BSTR fwBstrName = SysAllocString(L"{9F82CD7E-BA9F-48b7-BC82-02CCD524775A}"))
+		{
+			fOk = !fwOpenPort->put_Port(portNumber) &&
+				!fwOpenPort->put_Name(fwBstrName) &&
+				!fwOpenPort->put_Protocol(ipProtocol) &&
+				!fwOpenPorts->Add(fwOpenPort);
 
-      SysFreeString(fwBstrName);
-    }
+			SysFreeString(fwBstrName);
+		}
 
-    fwOpenPort->Release();
-  }
+		fwOpenPort->Release();
+	}
 
-  return fOk;
+	return fOk;
 }
 
 BOOL OpenPort(WORD portNumber)
 {
-  BOOL fOk = FALSE;
+	BOOL fOk = FALSE;
 
-  INetFwMgr* fwMgr;
-  INetFwPolicy* fwPolicy;
-  INetFwProfile* fwProfile;
-  INetFwOpenPorts* fwOpenPorts;
+	INetFwMgr* fwMgr;
+	INetFwPolicy* fwPolicy;
+	INetFwProfile* fwProfile;
+	INetFwOpenPorts* fwOpenPorts;
 
-  if (!CoCreateInstance(__uuidof(NetFwMgr), 0, CLSCTX_INPROC_SERVER, IID_PPV(fwMgr)))
-  {
-    if (!fwMgr->get_LocalPolicy(&fwPolicy))
-    {
-      if (!fwPolicy->get_CurrentProfile(&fwProfile))
-      {
-        if (!fwProfile->get_GloballyOpenPorts(&fwOpenPorts))
-        {
-          fOk = OpenPort(fwOpenPorts, portNumber, NET_FW_IP_PROTOCOL_UDP);
+	if (!CoCreateInstance(__uuidof(NetFwMgr), 0, CLSCTX_INPROC_SERVER, IID_PPV(fwMgr)))
+	{
+		if (!fwMgr->get_LocalPolicy(&fwPolicy))
+		{
+			if (!fwPolicy->get_CurrentProfile(&fwProfile))
+			{
+				if (!fwProfile->get_GloballyOpenPorts(&fwOpenPorts))
+				{
+					fOk = OpenPort(fwOpenPorts, portNumber, NET_FW_IP_PROTOCOL_UDP);
 
-          fwOpenPorts->Release();
-        }
+					fwOpenPorts->Release();
+				}
 
-        fwProfile->Release();
-      }
-      fwPolicy->Release();
-    }
-    fwMgr->Release();
-  }
+				fwProfile->Release();
+			}
+			fwPolicy->Release();
+		}
+		fwMgr->Release();
+	}
 
-  return fOk;
+	return fOk;
 }
-
 
 _NT_END
